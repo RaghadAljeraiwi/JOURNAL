b1df2052c09df5ccb72d3c5ce3c6ea8c20a66dd1
Raghad <raghad.waleedj@gmail.com>
2025-10-19 11:24:55 +0300

    Initial Commit

---
+++ JOURNAL/JOURNALApp.swift
@@ -0,0 +1,17 @@
+//
+//  JOURNALApp.swift
+//  JOURNAL
+//
+//  Created by رغد الجريوي on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct JOURNALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
