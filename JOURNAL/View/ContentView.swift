e55d07793261125e41963e0c4d17ee65596b756b
Raghad <raghad.waleedj@gmail.com>
2025-10-27 09:29:09 +0300

    pre-final

---
+++ JOURNAL/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  JOURNAL
+//
+//  Created by رغد الجريوي on 19/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SplashView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
